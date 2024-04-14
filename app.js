0ef00f1a70bc4e6ca631abda5455a7b50a930e0f
Ilya <honeyflym@gmail.com>
2024-04-14 21:43:57 +0300

    fix lint errors

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const { errors } = require("celebrate");
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/errorHandler");
-const { errors } = require("celebrate");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const { PORT = 3001 } = process.env;
